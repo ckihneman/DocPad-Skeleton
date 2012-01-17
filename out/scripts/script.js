16357d6811dd1780f7058c742de182ecfdb4831a
Chris Kihneman <ckihneman@gmail.com>
2012-01-16 23:18:49 -0800

    initial commit

---
+++ out/scripts/script.js
@@ -0,0 +1,9 @@
+(function() {
+
+  $(function() {
+    return $('#header').find('h1').on('click', function() {
+      return window.location = '/';
+    });
+  });
+
+}).call(this);
---
+++ src/documents/scripts/script.js.coffee
@@ -0,0 +1,6 @@
+
+$ ->
+
+	# Feel free to ditch this.
+	$( '#header' ).find( 'h1' ).on 'click', ->
+		window.location = '/'
